fc54ec228e5c99dc1fc4f9b2547093be1d725edf
itzhuang <itzhuangcn@gmail.com>
2022-05-03 19:33:59 +0800

    2th version

--- window.py
+++ window.py
@@ -14,10 +14,10 @@ class MainWindows(tk.Tk):
 
         self.title("三维重建系统")
         self.resizable(width=True, height=True) #是否可变窗口大小
-        self.geometry("1024x768")
+        self.geometry("960x640")
         # self.minsize(640, 320)
         self.tabControl = ttk.Notebook(self)  # Create Tab Control
-        self.tab1 = ttk.Frame(self.tabControl)  # Create a tab
+        self.tab1 = tk.Frame(self.tabControl)  # Create a tab
         self.menu_bar = Menu(self)  # Creating a Menu Bar
 
         self.init_ui()
@@ -39,22 +39,24 @@ class MainWindows(tk.Tk):
         self.init_menu()
 
     def init_tab1(self):
-        mighty = ttk.Frame(self.tab1)
+        mighty = tk.Frame(self.tab1)
         mighty.pack()
-        self.label1 = tk.Label(mighty, text='检测图片',font=('Noto Sans CJK SC Black',10), bg="Silver", padx=15, pady=15)
+        self.label1 = tk.Label(mighty, text='检测图片',font=('',11), bg="Silver", padx=15, pady=15)
         # self.label2 = tk.Label(mighty, text='检测图片2', bg="Silver", padx=15, pady=15)
         self.label1.grid(column=0, row=0, sticky='WN')
         # cself.label2.grid(column=1, row=0, sticky='W')
-        btn1 = ttk.Button(mighty, text="选择文件", command=self.select_btn_tab1, width=15)
-        btn2 = ttk.Button(mighty, text="获取结果", command=self.get_result1, width=15)
+        btn1 = tk.Button(mighty, text="选择文件", font=('',11) ,command=self.select_btn_tab1, width=15, height=2)
+        btn2 = tk.Button(mighty, text="获取结果",font=('',11) ,command=self.get_result1, width=15, height=2)
+        btn3 = tk.Button(mighty, text="查看结果", font=('', 11), command=self.see_result,width=15, height=2)
 
         btn1.grid(column=0, row=1, sticky='W')
         btn2.grid(column=0, row=2, sticky='W')
-        label3 = tk.Label(mighty, text='相似度(一般70%以上可以表示为同一个人):')
-        label3.grid(column=0, row=3, sticky='W')
+        btn3.grid(column=0,row=3, sticky='W')
+        label3 = tk.Label(mighty, text='运行信息:',font=('',11))
+        label3.grid(column=0, row=4, sticky='W')
         self.name = tk.StringVar()
         name_entered = ttk.Entry(mighty, width=12, textvariable=self.name)
-        name_entered.grid(column=1, row=3, sticky='W')  # align left/West
+        name_entered.grid(column=1, row=4, sticky='W')  # align left/West
 
 
 
@@ -76,7 +78,7 @@ class MainWindows(tk.Tk):
             return
         for i in range(length):
             img = Image.open(filename[i])
-            half_size = (256,256)
+            half_size = (1024,768)
             img.thumbnail(half_size, Image.ANTIALIAS)
             photo = ImageTk.PhotoImage(img)
             labels[i].configure(image=photo)
@@ -94,33 +96,21 @@ class MainWindows(tk.Tk):
         self.show_img([self.label1], self.select_file())
 
     def get_result1(self):
-
         img_path = self.selected_files[0]
 
-        print(1)
         parser = argparse.ArgumentParser()
         # parser.add_argument("--input", "-i", type=str, default="data/front3d-sample/test19.jpg")
         parser.add_argument("--input", "-i", type=str, default= img_path)
         parser.add_argument("--output", "-o", type=str, default="output/sample_0020/")
         parser.add_argument("--config-file", "-c", type=str, default="configs/front3d_sample.yaml")
-        parser.add_argument("--model", "-m", type=str, default="data/panoptic_front3d_v2.pth")
+        parser.add_argument("--model", "-m", type=str, default="data/panoptic_front3d.pth")
         parser.add_argument("opts", default=None, nargs=argparse.REMAINDER)
 
         args = parser.parse_args()
         test_net_single_image.main(args)
 
-
-
-        # # 计算两张图片的余弦相似度
-        # res = self.face_recog.cos_sim(v1, v2).tolist()[0][0]
-        # res = format(res * 100, '.2f')
-        # self.name.set(res + '%')
-
-    def select_btn_tab2(self):
-        self.show_img([self.label_rec], self.select_file())
-
-
-
+    def see_result(self):
+        filedialog.askopenfile(initialdir='~/panoptic-reconstruction-main/output/sample_0020')
 
     def _quit(self):
         self.quit()
